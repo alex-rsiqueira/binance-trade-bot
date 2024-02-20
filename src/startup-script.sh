79f716cf29dbc1408561ece99f227659708b2098
Aleesiqueira <alexsander_siqueira@outlook.com>
2024-02-20 02:33:56 +0000

    fix startup-script

--- src/startup-script.sh
+++ src/startup-script.sh
@@ -1,10 +1,9 @@
 # Install or update needed software
 sudo apt-get update
 sudo apt-get install -yq git supervisor python3 python3-dev python3-venv
-pip3 install --upgrade pip virtualenv
 
-sudo apt-get install wget
-wget https://bootstrap.pypa.io/get-pip.py
+sudo wget https://bootstrap.pypa.io/get-pip.py
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 sudo python3 get-pip.py
 
 # Fetch source code
@@ -18,8 +17,9 @@ sudo bash /opt/app/src/add-google-cloud-ops-agent-repo.sh --also-install
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3 /opt/app/env
-/bin/bash -c "source /opt/app/env/bin/activate"
+cd /opt/app
+sudo python3 -m venv env
+source env/bin/activate
 sudo /opt/app/env/bin/pip install -r /opt/app/src/requirements.txt
 
 # Set ownership to newly created account
